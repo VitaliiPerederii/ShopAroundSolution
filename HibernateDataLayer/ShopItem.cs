2f9cbac0929ef588a0f66103fe0f54870e64f7ac
VitaliiPerederii <vitalmbt@gmail.com>
2015-10-30 09:34:58 +0200

    30.10.15

---
+++ HibernateDataLayer/ShopItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+        
+//http://nhibernate.info/doc/tutorials/first-nh-app/your-first-nhibernate-based-application.html
+
+namespace HibernateDataLayer
+{
+    public partial class ShopItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public bool Available { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
